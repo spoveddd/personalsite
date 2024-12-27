3a18036eb3a190d06746170d5bdfd717277c3595
Spoved <spoved@proton.me>
2024-12-27 17:16:58 +0300

    new style feedback 4

--- view-feedback.php
+++ view-feedback.php
@@ -12,7 +12,7 @@ $result = $db->query('SELECT * FROM feedback ORDER BY created_at DESC');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Отзывы</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
     <section class="section">
