945410fe20ce09bdb4d1c593ce6f7c9605591660
Spoved <spoved@proton.me>
2024-12-28 10:40:48 +0300

    new structur project 7

--- assets/php/view-feedback.php
+++ assets/php/view-feedback.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Отзывы</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
     <section class="feedback-section">
